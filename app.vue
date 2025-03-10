5f8d6dc7fddff89c0c50807d0ab0dac3473195d6
maxxbit <maxielcade04@gmail.com>
2025-03-10 21:03:03 +0100

    gtag

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <script setup>
 import Toaster from '@/components/ui/toast/Toaster.vue'
+import { useHead } from '#imports'
+
+useHead({
+	script: [
+		{
+			src: `https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`,
+			async: true,
+		},
+		{
+			children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-XXXXXXXXXX', {
+          linker: { domains: ['maxxbit.es', 'thinkforward.maxxbit.es'] }
+        });
+      `,
+			type: 'text/javascript',
+		},
+	],
+})
 </script>
 <template>
 	<div class="bg-white dark:bg-slate-950 w-screen">
